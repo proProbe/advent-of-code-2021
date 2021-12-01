4f12321c1bba5840d4bcdf4ea4df59d94e8bf32f
proprobe <work.huy.tran@gmail.com>
2021-12-01 17:48:03 +0100

    [DAY1]: add solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,29 @@
 use anyhow::Result;
+use std::fs;
 use std::path::Path;
 
-pub fn main(_path: &Path) -> Result<(usize, Option<usize>)> {
-    Ok((0, None))
+fn parse_line(line: &str) -> usize {
+    line.parse::<usize>().expect("Failed to parse")
+}
+
+fn a(content: &str) -> usize {
+    let left = content.lines().map(parse_line);
+    let right = content.lines().skip(1).map(parse_line);
+
+    left.zip(right).filter(|(l, r)| r > l).count()
+}
+
+fn b(content: &str) -> usize {
+    let left = content.lines().map(parse_line);
+    let right = content.lines().skip(3).map(parse_line);
+
+    left.zip(right).filter(|(l, r)| r > l).count()
+}
+
+pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
+    let content = fs::read_to_string(path).expect("Failed to read");
+
+    Ok((a(&content), Some(b(&content))))
 }
 
 #[cfg(test)]
