3981186ab1be5d3cfdc63743abf956aa324ca9e2
proprobe <work.huy.tran@gmail.com>
2021-12-01 17:54:59 +0100

    [DAY1]: refactor

--- src/day1.rs
+++ src/day1.rs
@@ -6,24 +6,25 @@ fn parse_line(line: &str) -> usize {
     line.parse::<usize>().expect("Failed to parse")
 }
 
-fn a(content: &str) -> usize {
+fn count_depth_increase(window: usize, content: &str) -> usize {
     let left = content.lines().map(parse_line);
-    let right = content.lines().skip(1).map(parse_line);
+    let right = content.lines().skip(window).map(parse_line);
 
     left.zip(right).filter(|(l, r)| r > l).count()
 }
 
-fn b(content: &str) -> usize {
-    let left = content.lines().map(parse_line);
-    let right = content.lines().skip(3).map(parse_line);
+fn part_one(content: &str) -> usize {
+    count_depth_increase(1, content)
+}
 
-    left.zip(right).filter(|(l, r)| r > l).count()
+fn part_two(content: &str) -> usize {
+    count_depth_increase(3, content)
 }
 
 pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
     let content = fs::read_to_string(path).expect("Failed to read");
 
-    Ok((a(&content), Some(b(&content))))
+    Ok((part_one(&content), Some(part_two(&content))))
 }
 
 #[cfg(test)]
